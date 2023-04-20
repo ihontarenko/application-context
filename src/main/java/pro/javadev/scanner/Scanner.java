0e4466316d25e6838eeaabbabef55e75e6ab6edc
Ivan Hontarenko <ihontarenko@gmail.com>
2023-04-20 10:50:03 +0300

    improve scanner abstrations

--- src/main/java/pro/javadev/scanner/Scanner.java
+++ src/main/java/pro/javadev/scanner/Scanner.java
@@ -1,6 +1,9 @@
 package pro.javadev.scanner;
 
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -15,6 +18,16 @@ public interface Scanner<T> {
     }
 
     default Set<T> scan(URL resource, String name, ClassLoader loader) {
+        Set<T> result = null;
+
+        try {
+            result = scan(Paths.get(resource.toURI()), name, loader);
+        } catch (URISyntaxException ignore) { }
+
+        return result;
+    }
+
+    default Set<T> scan(Path path, String name, ClassLoader loader) {
         throw new UnsupportedOperationException(UNSUPPORTED_FOR_CLASS_MESSAGE + this.getClass().getName());
     }
 
